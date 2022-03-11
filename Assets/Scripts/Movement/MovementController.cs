f034f8a9628eaea312b69f0529986ebbfed6c82a
Tunahan <karakoktunahan78@gmail.com>
2022-03-11 16:36:13 +0300

    State manager and movement system added

--- Assets/Scripts/Movement/MovementController.cs
+++ Assets/Scripts/Movement/MovementController.cs
@@ -2,17 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementController : MonoBehaviour
+public class MovementController : Singleton<MovementController>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float _limitX = 2;
+    [SerializeField] private float _xSpeed = 25;
+    [SerializeField] private float _forwardSpeed = 2;
+    private float _lastTouchedX;
+    
     void Update()
     {
-        
+        // We check game state, if state in game we can move 
+        if (StateManager.Instance.State == State.InGame)
+        {
+            float _touchXDelta = 0;
+            float _newX = 0;
+
+            #region Unity Editör Movement
+#if UNITY_EDITOR
+            if (Input.touchCount > 0)
+            {
+                if (Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    _lastTouchedX = Input.GetTouch(0).position.x;
+                }
+                else if (Input.GetTouch(0).phase == TouchPhase.Moved)
+                {
+                    _touchXDelta = 5 * (Input.GetTouch(0).position.x - _lastTouchedX) / Screen.width;
+                    _lastTouchedX = Input.GetTouch(0).position.x;
+                }
+            }
+#endif
+            #endregion
+
+            #region Android Movement
+            else if (Input.GetMouseButton(0))
+            {
+                _touchXDelta = Input.GetAxis("Mouse X");
+            }
+            #endregion
+
+            _newX = transform.position.x + _xSpeed * _touchXDelta * Time.deltaTime;
+            // we limited horzontal movement
+            _newX = Mathf.Clamp(_newX, -_limitX, _limitX);
+
+            Vector3 newPosition = new Vector3(_newX, transform.position.y, transform.position.z + _forwardSpeed * Time.deltaTime);
+            transform.position = newPosition;
+
+        }
     }
+
 }
