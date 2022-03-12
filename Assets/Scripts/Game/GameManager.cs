38ee8f94a6c90a1cd6b646a61af1eea4e74e4330
Tunahan <karakoktunahan78@gmail.com>
2022-03-12 15:09:53 +0300

    GameManager added and collision functions writed for money

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    #region Public Fields
+    public Vector3 offSetY;
+    public Transform parent, stackPosition;
+    #endregion
+
+    #region Private Fields
+    private int moneyCounter; // total money size
+    private int amountOfMoney; // total money amount
+    #endregion
+
+    #region EnCapsulation
+    public int AmountOfMoney { get => amountOfMoney; set => amountOfMoney = value; }
+    public int MoneyCounter { get => moneyCounter; set => moneyCounter = value; }
+    #endregion
+
+    private void Start()
+    {
+        moneyCounter = 0;
+        amountOfMoney = 0;
+    }
+
+
+    #region Money Functions
+    /// <summary>
+    /// we adding new amount if we collise in game money
+    /// </summary>
+    /// <param name="amount"> adding value </param>
+    public void AddMoney(int amount)
+    {
+        amountOfMoney += amount;
+    }
+
+    /// <summary>
+    /// subtract the value from the total money 
+    /// </summary>
+    /// <param name="amount"> minus value </param>
+    public void MinusMoney(int amount)
+    {
+        amountOfMoney -= amount;
+    }
+
+    /// <summary>
+    /// We calculated new stack position for new money 
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 NewStackPosition()
+    {
+        Vector3 newPosition = MoneyCounter * GameManager.Instance.offSetY;
+        return newPosition;
+    }
+
+    /// <summary>
+    /// we do the actions that will take place in collision control
+    /// </summary>
+    /// <param name="money">hit object</param>
+    public void CollisionMoneyFunction(Transform money)
+    {
+        money.transform.SetParent(parent);
+        AddMoney(10);
+        MoneyCounter++;
+        money.transform.position = stackPosition.position + NewStackPosition();
+        
+    }
+    #endregion
+
+}
---
+++ Assets/Scripts/Player/PlayerCollisionController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionController : MonoBehaviour
+{
+    
+    
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Money"))
+        {
+            other.tag = "Untagged";
+            GameManager.Instance.CollisionMoneyFunction(other.transform);
+            Debug.Log(GameManager.Instance.AmountOfMoney);
+        }
+    }
+}
